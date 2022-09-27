27cbbbfdf69293cf3892a34b2dceccf4dead658a
Fabio Evangelista <fabioevangelista_@hotmail.com>
2022-09-26 21:04:00 -0300

    templates dos pedidos criados e funcionais

--- src/main/java/com/evangelista/costura/controller/PedidoController.java
+++ src/main/java/com/evangelista/costura/controller/PedidoController.java
@@ -4,58 +4,58 @@ package com.evangelista.costura.controller;
 import com.evangelista.costura.model.Pedido;
 import com.evangelista.costura.model.services.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 public class PedidoController {
 
     @Autowired
     PedidoService pedidoService;
 
-    // registra um pedido
-    @PostMapping("/gravarpedido")
-    public String gravarPedido(@RequestBody Pedido pedido){
 
+    @GetMapping("/postpedido")
+    public String getPostPedido(){
+        return "NewPedido";
+    }
+
+    // registra um pedido
+    @PostMapping("/postpedido")
+    public String postPedido(@Valid Pedido pedido, BindingResult result, RedirectAttributes redirectAttributes){
+
+        if (result.hasErrors()){
+            redirectAttributes.addFlashAttribute("m", "Verifique se os campos obrigatórios foram preenchidos.");
+            return "redirect:/postpedido";
+        }
         pedido.setData(LocalDate.now());
         pedidoService.save(pedido);
-
-        return "Pedido registrado";
+        return "redirect:/getpedidos";
     }
 
     // retorna todos os pedidos
-    @GetMapping("/pedidos")
-    public List<Pedido> getPedidos(){
+    @GetMapping("/getpedidos")
+    public ModelAndView getPedidos(){
 
-        return pedidoService.findAll();
+        ModelAndView modelAndView = new ModelAndView("Pedidos");
+        List<Pedido> pedidos = pedidoService.findAll();
+        modelAndView.addObject("pedidos", pedidos);
+        return modelAndView;
 
     }
 
-    @PutMapping("/updatepedido/{id}")
-    public String putPedido(@PathVariable("id") Long id, @RequestBody Pedido pedido){
-
-        Optional<Pedido> pedById = pedidoService.findById(id);
-
-        if (pedById.isPresent()){
-            pedido.setTipo(pedido.getTipo());
-            pedido.setMarca(pedido.getMarca());
-            pedido.setValorPeca(pedido.getValorPeca());
-            pedido.setData(LocalDate.now());
-        }
-        pedidoService.save(pedido);
-
-        return "Pedido atualizado";
-    }
-
-    @DeleteMapping("/deletepedido/{id}")
+    //deleta um pedido em específico
+    @GetMapping("/deletepedido/{id}")
     public String deletePedido(@PathVariable("id") Long id){
-
         pedidoService.delete(id);
-
-        return "Pedido deletado";
+        return "redirect:/getpedidos";
     }
 
 }
